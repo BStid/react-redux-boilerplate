e45aa986d43a553db5871bb1d19fd54174ec0b29
Ben <ben@benstidham.com>
2018-10-16 23:21:11 -0500

    changed state name

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -5,21 +5,22 @@ const UPDATE_SOMETHING = "UPDATE_SOMETHING";
 
 //Create InitialState
 const initialState = {
-  stuff: []
+  stuff: [],
+  actionKey: ""
 };
 
 //add some SWITCH CASES in the REDUCER
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case UPDATE_SOMETHING:
-      return { ...state, actionType: action.payload };
+      return { ...state, actionKey: action.payload };
   }
 }
 
 //add some ACTION CREATORS
-export function somethingFun(actionType) {
+export function somethingFun(actionKey) {
   return {
     type: UPDATE_SOMETHING,
-    payload: actionType
+    payload: actionKey
   };
 }
