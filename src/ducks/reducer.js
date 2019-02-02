d59da9847eda8d1651500408218d993aeb45df29
Ben Stidham <41093554+BStid@users.noreply.github.com>
2019-02-02 01:14:49 -0600

    Update reducer.js

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -5,23 +5,25 @@ const UPDATE_SOMETHING = "UPDATE_SOMETHING";
 
 //Create InitialState
 const initialState = {
-  actionKey: ""
+  inputValue: ""
 };
+
+//add some ACTION CREATORS
+export function updateSomething(input) {
+  return {
+    type: UPDATE_SOMETHING,
+    payload: input
+  };
+}
 
 //add some SWITCH CASES in the REDUCER
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case UPDATE_SOMETHING:
-      return { ...state, actionKey: action.payload };
+      return { ...state, inputValue: action.payload };
+    default:
+      return state;
   }
-}
-
-//add some ACTION CREATORS
-export function somethingFun(actionKey) {
-  return {
-    type: UPDATE_SOMETHING,
-    payload: actionKey
-  };
 }
 
 //axios request - - install promise middleware
